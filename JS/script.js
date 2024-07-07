021455aad77eb37507d4c6332e371c07c2c6c767
PChArtDesign <paulinechailloux@rocketmail.com>
2024-07-07 10:24:10 +0200

    Modification de la page projet artistique

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,32 @@
-    function showImage(imageSrc, imageText) {
-    // Récupérer les éléments de l'image et du texte
-        const largeImage = document.getElementById('largeImage');
-        const imageTextElement = document.getElementById('imageText');
-        const imageDisplay = document.getElementById('imageDisplay');
+document.addEventListener('DOMContentLoaded', function() {
+    const categoryButtons = document.querySelectorAll('.category-btn');
+    const thumbnailsContainers = document.querySelectorAll('.thumbnails-containerG');
+    const displayedImage = document.getElementById('displayed-image');
+    const imageTitle = document.getElementById('image-title');
+    const imageDescription = document.getElementById('image-description');
 
-        // Mettre à jour l'image et le texte
-        largeImage.src = imageSrc;
-        imageTextElement.textContent = imageText;
+    categoryButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const category = button.getAttribute('data-category');
+            thumbnailsContainers.forEach(containerG => {
+                if (containerG.id === 'thumbnails-' + category) {
+                    containerG.style.display = containerG.style.display === 'flex' ? 'none' : 'flex';
+                } else {
+                    containerG.style.display = 'none';
+                }
+            });
+        });
+    });
 
-        // Afficher la section d'image si elle est cachée
-        imageDisplay.style.display = 'block';
-    }
+    const thumbnails = document.querySelectorAll('.thumbnail');
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function() {
+            const src = thumbnail.getAttribute('src');
+            const alt = thumbnail.getAttribute('alt');
+            const description = thumbnail.getAttribute('data-description');
+            displayedImage.setAttribute('src', src);
+            imageTitle.textContent = alt;
+            imageDescription.innerHTML = description; // Utilisation de innerHTML pour inclure les balises <br>
+        });
+    });
+});
