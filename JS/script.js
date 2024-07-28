cd0764610a5fde71d4e2925ec43b65dc949c0d59
PChArtDesign <paulinechailloux@rocketmail.com>
2024-07-28 12:48:36 +0200

    WIP Graphisme

--- JS/script.js
+++ JS/script.js
@@ -29,5 +29,66 @@ document.addEventListener('DOMContentLoaded', function() {
             imageDescription.innerHTML = description; // Utilisation de innerHTML pour inclure les balises <br>
         });
     });
+
+
+        const carouselInner = document.querySelector('.carousel-inner');
+        const images = [
+            'Images/Graphisme/LadyRetro.PNG', 'Images/Graphisme/OsteoFlor.PNG', 'Images/Graphisme/JMC.PNG', 'Images/Graphisme/OpenCheer.PNG', 'Images/Graphisme/DCBO.PNG',
+            'Images/Graphisme/JustMarried.PNG', 'Images/Graphisme/2CZelles.PNG', 'Images/Graphisme/RCMulti.PNG', 'Images/Graphisme/nathy.png', 'Images/Graphisme/BAL.PNG'
+        ];
+        
+        // Cloner les premières et dernières images pour un effet infini
+    const totalItems = images.length;
+    const itemsToShow = 5;
+
+    // Ajouter les images au début et à la fin pour créer un effet infini
+    const startClone = images.slice(-itemsToShow);
+    const endClone = images.slice(0, itemsToShow);
+
+    const allImages = [...startClone, ...images, ...endClone];
+
+    allImages.forEach((imgSrc, index) => {
+        const itemDiv = document.createElement('div');
+        itemDiv.classList.add('carousel-item');
+        const img = document.createElement('img');
+        img.src = imgSrc;
+        img.alt = `Image ${index + 1}`;
+        itemDiv.appendChild(img);
+        carouselInner.appendChild(itemDiv);
+    });
+
+    let currentIndex = itemsToShow; // Commence à la première image réelle
+    const items = document.querySelectorAll('.carousel-item');
+
+    // Initialiser la position sans transition
+    carouselInner.style.transition = 'none';
+    carouselInner.style.transform = `translateX(${-currentIndex * 20}%)`;
     
+    // Ajouter un délai avant de réactiver la transition
+    setTimeout(() => {
+        carouselInner.style.transition = 'transform 0.5s ease-in-out';
+    }, 50);
+
+    function showNextImage() {
+        currentIndex++;
+        updateCarousel();
+        if (currentIndex === totalItems + itemsToShow) {
+            setTimeout(() => {
+                carouselInner.style.transition = 'none';
+                currentIndex = itemsToShow;
+                updateCarousel();
+                setTimeout(() => {
+                    carouselInner.style.transition = 'transform 0.5s ease-in-out';
+                }, 50);
+            }, 500);
+        }
+    }
+
+    function updateCarousel() {
+        const newTransformValue = -currentIndex * 20; // 20% corresponds to one image width
+        carouselInner.style.transform = `translateX(${newTransformValue}%)`;
+    }
+
+    setInterval(showNextImage, 3000); // Change l'image toutes les 3 secondes
+
 });
